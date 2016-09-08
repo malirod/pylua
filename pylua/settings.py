459dfe6e0d02503d9c6b360f5d7a58c473b0997b
Rodion Malinovsky <rodionmalino@gmail.com>
2016-09-08 09:02:49 +0300

    Add default settings file

---
+++ pylua/settings.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+#-*- coding: utf-8 -*-
+
+LOG_CONFIG = 'log.config'
+
+#Load custom\local settings if exists
+try:
+    from local_settings import *  # NOQA
+except ImportError:
+    pass
