d2dcd514bf839c8b96c9cae10c7571ebd0fae0b9
Rodion Malinovsky <rodionmalino@gmail.com>
2016-09-08 09:12:19 +0300

    Add logger.py with init_logger function. Not used at the moment.

---
+++ pylua/util/logger.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+#-*- coding: utf-8 -*-
+
+from pylue.settings import LOG_CONFIG
+
+
+def init_logging():
+    from logging.config import fileConfig
+
+    fileConfig(LOG_CONFIG)
